9fe3136e87e05a80eeee6bfad5e2634e23fe4ec5
MohdAdnanKhan001 <adnankhanmak1786@gmail.com>
2025-07-15 20:43:54 +0530

    Typo

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter, Route, createRoutesFromElements } from 'react-router-dom';
-
 import App from './App.jsx';
 import Layout from './Layout.jsx';
 import Home from './components/Home/Home.jsx';
