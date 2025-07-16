2c0f9563818f5150ede2004f3298e0e584b1bc18
MohdAdnanKhan001 <adnankhanmak1786@gmail.com>
2025-07-16 23:17:53 +0530

    Initaited

---
+++ src/components/MailProtest/MailProtest.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MailProtest() {
+  return (
+    <div>
+      Mail protest feature
+    </div>
+  )
+}
+
+export default MailProtest
---
+++ src/components/Suggestions/Suggestions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Suggestions() {
+  return (
+    <div>
+      Suggestions Page - Shamoon and ashrah's  B2B idea
+    </div>
+  )
+}
+
+export default Suggestions
