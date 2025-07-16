39a1cfcbe911c59c79d446dc7842379e3a7b882e
MohdAdnanKhan001 <adnankhanmak1786@gmail.com>
2025-07-16 23:17:19 +0530

    Initaited

--- src/components/Boycott/Boycott.jsx
+++ src/components/Boycott/Boycott.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Boycott() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-semibold text-gray-800">About Page</h1>
+      <h1 className="text-3xl font-semibold text-gray-800">Boycott Page</h1>
     </div>      
   )
 }
