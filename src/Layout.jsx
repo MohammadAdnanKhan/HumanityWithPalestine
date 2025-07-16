488997717acda7fdc2d6ec53cf7ba7b03eb95831
MohdAdnanKhan001 <adnankhanmak1786@gmail.com>
2025-07-16 23:20:01 +0530

    Added routes and imports

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
 
 function Layout() {
   return (
     <>
-    <Header />
+    <Navbar />
     <Outlet />
     <Footer />
     </>
