b174dcedc2bd4ce5f0aa96906f89c0694e8e786a
MohdAdnanKhan001 <adnankhanmak1786@gmail.com>
2025-07-18 19:40:09 +0530

    Logo changed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo1.png";
 import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
